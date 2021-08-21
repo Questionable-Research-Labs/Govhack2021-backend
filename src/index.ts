78d156d3a5d6b3ce3bd708cea604c5de951db1cb
Nathan Hare <me@laspruca.nz>
2021-08-22 01:35:02 +1200

    Add firebase for backend token caching

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import admin from "firebase-admin";
 dotenv.config();
 
 const app = express();
+
 app.use(
   cors({
     origin: [
@@ -16,17 +17,34 @@ app.use(
     ],
   })
 );
+
 const port = 5000;
 
 let datePushed: string = "";
 let registrationTokens: string[] = [];
 
+// Pro tip: Generate a personal access token!
 const octokit = new Octokit({ auth: env.API_KEY, userAgent: "toiv0.1.0" });
 const serviceAccount = JSON.parse(env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
+const db = admin.firestore();
+
+// Decide wether in dev enviourment or production env (if there ever was a difference)
+const docName = env.DEV ? "devTokens" : "tokens";
+
+// Get a reference to the firebase tokens
+let docRef = db.collection("registrationTokens").doc(docName);
+
+// Get tokens from firestore
+(async () => {
+  let doc = await docRef.get();
+  registrationTokens = doc.data()["tokens"];
+  console.log("Fetched tokens from firesotre", registrationTokens);
+})();
 
 interface GitHubFileRequest {
   owner: string;
@@ -35,6 +53,7 @@ interface GitHubFileRequest {
   branch: string;
 }
 
+// Function to build query for GitHub to get latest commit date
 const createRequest = (request: GitHubFileRequest) => `{
   repository(owner: "${request.owner}", name: "${request.repoName}") {
     ref(qualifiedName: "refs/heads/${request.branch}") {
@@ -56,17 +75,19 @@ const createRequest = (request: GitHubFileRequest) => `{
 const updateDatePushed = async () => {
   let request: any = await octokit.graphql(
     createRequest({
-      owner: "LaSpruca",
-      repoName: "TheBigSend",
-      path: "README.md",
-      branch: "master",
+      owner: "minhealthnz",
+      repoName: "nz-covid-data",
+      path: "locations-of-interest/august-2021/locations-of-interest.geojson",
+      branch: "main",
     })
   );
 
+  // Cancer.
   if (request?.repository?.ref?.target?.history?.edges.length > 0) {
     let newDatePushed =
       request?.repository?.ref?.target?.history?.edges[0]?.node?.committedDate;
 
+    // Check to see if the date has updated
     if (
       datePushed !== "" &&
       newDatePushed !== datePushed &&
@@ -108,20 +129,32 @@ app.get("/updated", (req, res) => {
   );
 });
 
-app.post("/push-notification/:token", (req, res) => {
+app.post("/push-notification/:token", async (req, res) => {
   let token = req.params.token;
+  // Add to list of tokens
   registrationTokens.push(token);
   console.log("Adding token ", token);
   res.send("Ok");
+
+  // Update firesotre
+  await docRef.set({
+    tokens: registrationTokens,
+  });
 });
 
-app.delete("/push-notification/:token", (req, res) => {
+app.delete("/push-notification/:token", async (req, res) => {
   let token = req.params.token;
+  // Remove from list of tokens
   registrationTokens = registrationTokens.filter((e) => e != token);
   console.log("Removing token", token, "\nTokens are", registrationTokens);
   res.send("Ok");
+
+  // Update firestore
+  await docRef.set({
+    tokens: registrationTokens,
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Toi Backend running on http://localhost:${port}`);
 });
